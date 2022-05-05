ea7b0cbc66a7638b0e1e01f6e6cf3fa7b0f626cc
Epgor <61190141+Epgor@users.noreply.github.com>
2022-05-05 15:09:59 +0200

    nowe fakty

--- payFacts.py
+++ payFacts.py
@@ -16,3 +16,15 @@ class Premie(Fact):
 
 class Pochwala(Fact):
     pass
+
+class Szkolenia(Fact):
+    pass
+
+class Wyksztalcenie(Fact):
+    pass
+
+class Urlop(Fact):
+    pass
+
+class DoswiadczenieWBranzy(Fact):
+    pass
