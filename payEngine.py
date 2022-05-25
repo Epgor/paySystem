24ce40cfd9df36660693c1b9d67d93a36065042b
Epgor <61190141+Epgor@users.noreply.github.com>
2022-05-25 14:17:26 +0200

    pdf

--- payEngine.py
+++ payEngine.py
@@ -4,61 +4,234 @@ class WyplataSilnik(KnowledgeEngine):
     def __init__(self):
         super().__init__()
         self.wyplata=1
-        self.godz=1
-        self.pln=1
-        self.dodatek=1
-
+        self.dodatek=0
+        print("Obliczanie pensji pracownika...")
+    """
     @Rule(AS.ileGodz << GodzinPracy(godz=P(lambda x: x > 0)))
     def zlicz_godziny(self, ileGodz):
         self.setGodz(ileGodz['godz'])
-        print("są godziny", ileGodz['godz'])
+        print("Pracownik przepracował: ", ileGodz['godz'], "godzin")
 
     @Rule(AS.zaGodz << PensjaNaGodzine(zl=P(lambda x: x > 0)))
     def przelicz_pensje(self, zaGodz):
         self.setStawka(zaGodz['zl'])
-        print("za godzine: ", zaGodz['zl'])
-        
-    @Rule(LataPracy(lat=P(lambda x: x > 15)))
+        print("Stawka za godzinę pracownika: ", zaGodz['zl'])
+    """
+    @Rule(LataPracy(lat=P(lambda x: x >= 10)))
     def premia_za_bd_lata(self):
         self.wyplata *= 1.15
-        print("bardzo długo pracuje")
-        self.ile_wyplaty()
-
-    @Rule(LataPracy(lat=P(lambda x: x >= 10) & P(lambda x: x <= 15)))
-    def premia_za_d_lata(self):
-        self.wyplata *= 1.10
-        print("długo pracuje")
-        self.ile_wyplaty()
+        print("Pracownik ma bardzo duże doświadczenie")
 
     @Rule(LataPracy(lat=P(lambda x: x >= 5) & P(lambda x: x < 10)))
     def premia_za_sd_lata(self):
         self.wyplata *= 1.05
-        print("średnio długo pracuje")
-        self.ile_wyplaty()
+        print("Pracownik ma duże doświadczenie")
 
-    @Rule(LataPracy(lat=P(lambda x: x >= 2) & P(lambda x: x < 5)))
+    @Rule(LataPracy(lat=P(lambda x: x >= 1) & P(lambda x: x < 5)))
     def premia_za_s_lata(self):
         self.wyplata *= 1.02
-        print("średnio pracuje : mid")
-        self.ile_wyplaty()
+        print("Pracownik ma małe doświadczenie")
 
-    @Rule(LataPracy(lat=P(lambda x: x >= 1) & P(lambda x: x < 2)))
+    @Rule(LataPracy(lat=P(lambda x: x < 1)))
     def premia_za_k_lata(self):
-        self.wyplata *= 1.01
-        print("krótko pracuje")
-        self.ile_wyplaty()
+        print("Pracownik bez doświadczenia")
 
-    @Rule(Pochwala(poch=1) | Pochwala(poch=2))
-    def dodaj_premie_1(self):
-        self.dodatek +=200
-        print("jest dodatek")
+    @Rule(Stanowisko(stan="Stażysta") & Etat(wymiar="Cały Etat"))
+    def podstawaStaz(self):
+        wyplata =  Staz.stawka * Caly.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
 
-    def setGodz(self, godz):
-        self.godz = godz
-    def setStawka(self, pln):
-        self.pln = pln
-    def setWyplata(self):
-        self.wyplata *= self.godz * self.pln
+    @Rule(Stanowisko(stan="Stażysta") & Etat(wymiar="3/4 Etatu"))
+    def podstawaStaz1(self):
+        wyplata = Staz.stawka * TrzyCwierc.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Stażysta") & Etat(wymiar="1/2 Etatu"))
+    def podstawaStaz2(self):
+        wyplata =  Staz.stawka * Polowa.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Stażysta") & Etat(wymiar="1/4 Etatu"))
+    def podstawaStaz3(self):
+        wyplata =  Staz.stawka * Cwierc.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Młodszy Specjalista") & Etat(wymiar="Cały Etat"))
+    def podstawaJunior(self):
+        wyplata =  Junior.stawka * Caly.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Młodszy Specjalista") & Etat(wymiar="3/4 Etatu"))
+    def podstawaJunior1(self):
+        wyplata =  Junior.stawka * TrzyCwierc.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Młodszy Specjalista") & Etat(wymiar="1/2 Etatu"))
+    def podstawaJunior2(self):
+        wyplata =  Junior.stawka * Polowa.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Młodszy Specjalista") & Etat(wymiar="1/4 Etatu"))
+    def podstawaJunior3(self):
+        wyplata =  Junior.stawka * Cwierc.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+
+    @Rule(Stanowisko(stan="Specjalista") & Etat(wymiar="Cały Etat"))
+    def podstawaMid(self):
+        wyplata =  Mid.stawka * Caly.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Specjalista") & Etat(wymiar="3/4 Etatu"))
+    def podstawaMid1(self):
+        wyplata =  Mid.stawka * TrzyCwierc.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Specjalista") & Etat(wymiar="1/2 Etatu"))
+    def podstawaMid2(self):
+        wyplata =  Mid.stawka * Polowa.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Specjalista") & Etat(wymiar="1/4 Etatu"))
+    def podstawaMid3(self):
+        wyplata =  Mid.stawka * Cwierc.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+
+
+    @Rule(Stanowisko(stan="Starszy Specjalista") & Etat(wymiar="Cały Etat"))
+    def podstawaSenior(self):
+        wyplata =  Senior.stawka * Caly.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Starszy Specjalista") & Etat(wymiar="3/4 Etatu"))
+    def podstawaSenior1(self):
+        wyplata =  Senior.stawka * TrzyCwierc.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Starszy Specjalista") & Etat(wymiar="1/2 Etatu"))
+    def podstawaSenior2(self):
+        wyplata =  Senior.stawka * Polowa.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+
+    @Rule(Stanowisko(stan="Starszy Specjalista") & Etat(wymiar="1/4 Etatu"))
+    def podstawaSenior3(self):
+        wyplata =  Senior.stawka * Cwierc.godzin
+        self.wyplata *= wyplata
+        print("Podstawowa Wypłata: ", wyplata)
+    ####################################################################
+
+    @Rule(AS.ileGodz << Nadgodziny(godz=P(lambda x: x > 0)) & Stanowisko(stan="Starszy Specjalista"))
+    def nadgodzSenior(self, ileGodz):
+        dodatek = ileGodz['godz'] * Senior.nadgodzina
+        self.dodatek += dodatek
+        print("Dodatek za nadgodziny: ", dodatek)
+
+    @Rule(AS.ileGodz << Nadgodziny(godz=P(lambda x: x > 0)) & Stanowisko(stan="Specjalista"))
+    def nadgodzMid(self, ileGodz):
+        dodatek = ileGodz['godz'] * Mid.nadgodzina
+        self.dodatek += dodatek
+        print("Dodatek za nadgodziny: ", dodatek)
+
+    @Rule(AS.ileGodz << Nadgodziny(godz=P(lambda x: x > 0)) & Stanowisko(stan="Młodszy Specjalista"))
+    def nadgodzJunior(self, ileGodz):
+        dodatek = ileGodz['godz'] * Junior.nadgodzina
+        self.dodatek += dodatek
+        print("Dodatek za nadgodziny: ", dodatek)
+
+    @Rule(AS.ileGodz << Nadgodziny(godz=P(lambda x: x > 0)) & Stanowisko(stan="Stażysta"))
+    def nadgodzStaz(self, ileGodz):
+        dodatek = ileGodz['godz'] * Staz.nadgodzina
+        self.dodatek += dodatek
+        print("Dodatek za nadgodziny: ", dodatek)
+    ################################################################################
+    @Rule(AS.ilePoch << Pochwala(poch=P(lambda x: x > 0)) & Stanowisko(stan="Stażysta"))
+    def pochwala(self, ilePoch):
+        dodatek = ilePoch['poch'] * Staz.stawka * 3
+        self.dodatek += dodatek
+        print("Dodatek za pochwały: ", dodatek)
+
+    @Rule(AS.ilePoch << Pochwala(poch=P(lambda x: x < 0)) & Stanowisko(stan="Stażysta"))
+    def uwaga(self, ilePoch):
+        dodatek = ilePoch['poch'] * Staz.stawka * 3
+        self.dodatek += dodatek
+        print("Potrącenie za uwagi: ", dodatek)
+
+
+    @Rule(AS.ilePoch << Pochwala(poch=P(lambda x: x > 0)) & Stanowisko(stan="Młodszy Specjalista"))
+    def pochwala1(self, ilePoch):
+        dodatek = ilePoch['poch'] * Junior.stawka * 3
+        self.dodatek += dodatek
+        print("Dodatek za pochwały: ", dodatek)
+
+    @Rule(AS.ilePoch << Pochwala(poch=P(lambda x: x < 0)) & Stanowisko(stan="Młodszy Specjalista"))
+    def uwaga1(self, ilePoch):
+        dodatek = ilePoch['poch'] * Junior.stawka * 3
+        self.dodatek += dodatek
+        print("Potrącenie za uwagi: ", dodatek)
+
+
+    @Rule(AS.ilePoch << Pochwala(poch=P(lambda x: x > 0)) & Stanowisko(stan="Specjalista"))
+    def pochwala2(self, ilePoch):
+        dodatek = ilePoch['poch'] * Mid.stawka * 3
+        self.dodatek += dodatek
+        print("Dodatek za pochwały: ", dodatek)
+
+    @Rule(AS.ilePoch << Pochwala(poch=P(lambda x: x < 0)) & Stanowisko(stan="Specjalista"))
+    def uwaga2(self, ilePoch):
+        dodatek = ilePoch['poch'] * Mid.stawka * 3
+        self.dodatek += dodatek
+        print("Potrącenie za uwagi: ", dodatek)
+
+
+    @Rule(AS.ilePoch << Pochwala(poch=P(lambda x: x > 0)) & Stanowisko(stan="Starszy Specjalista"))
+    def pochwala3(self, ilePoch):
+        dodatek = ilePoch['poch'] * Senior.stawka * 3
+        self.dodatek += dodatek
+        print("Dodatek za pochwały: ", dodatek)
+
+    @Rule(AS.ilePoch << Pochwala(poch=P(lambda x: x < 0)) & Stanowisko(stan="Starszy Specjalista"))
+    def uwaga3(self, ilePoch):
+        dodatek = ilePoch['poch'] * Senior.stawka * 3
+        self.dodatek += dodatek
+        print("Potrącenie za uwagi: ", dodatek)
+
+    @Rule(AS.ileGodz << DelegacjaGodz(godz=P(lambda x : x > 0)) & AS.ileProc << DelegacjaProc(proc=P(lambda x : x > 0)))
+    def deleg(self, ileGodz, ileProc):
+        dodatek = 100 * (ileGodz['godz']/24) * (ileProc['proc']/100)
+        dodatek = round(dodatek, 2)
+        self.dodatek += dodatek
+        print("Dieta za delegację: ", dodatek)
+
+    @Rule(AS.ileDod << DelegacjaDod(dod=P(lambda x :  x > 0)))
+    def dodatek(self, ileDod):
+        dodatek = ileDod['dod']
+        self.dodatek += dodatek
+        print("Dodatek za poniesione koszty: ", dodatek)       
+
     def ile_wyplaty(self):
-        self.setWyplata()
-        print(self.wyplata)
+        print("Sugerowana wypłata pracownika to: ", self.wyplata + self.dodatek)
+
+"""
+engine = WyplataSilnik()
+engine.reset()
+engine.declare(Pochwala(poch=1), Stanowisko(stan="Stażysta"), Etat(wymiar="Cały Etat"))
+
+engine.run()
+engine.ile_wyplaty()
+"""
---
+++ payPdf.py
@@ -0,0 +1,102 @@
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+import datetime
+
+class PayPdfGenerator:
+    def __init__(self):
+        pdfmetrics.registerFont(TTFont('Vera', 'Vera.ttf'))
+        self.date = datetime.date.today()
+        self.time = datetime.datetime.now()
+        self.hour = str(self.time.hour)
+        if len(self.hour)==1:
+            self.hour = "0"+self.hour
+        self.minutes = str(self.time.minute)
+        if len(self.minutes)==1:
+            self.minutes = "0"+self.minutes
+        self.second = str(self.time.minute)
+        if len(self.second)==1:
+            self.second = "0"+self.second 
+        self.hourP = f"{str(self.hour)}:{str(self.minutes)}:{str(self.second)}" 
+        self.stanowisko = "Senior"
+        self.wymiarPracy = "Cały Etat"
+        self.lataPracy = "12"
+        self.nadgodziny = "50"
+        self.delegacjaGodz = "30"
+        self.dodatkoweKoszty = "500"
+        self.uwagi = "-34"
+        self.wynikDziałania = ""
+        
+        
+    def konfiguracja(self, stanowisko, wymiarPracy, lataPracy, nadgodziny,\
+         delegacjaGodz, dodatkoweKoszty, uwagi):
+        self.stanowisko = str(stanowisko)
+        self.wymiarPracy = str(wymiarPracy)
+        self.lataPracy = str(lataPracy)
+        self.nadgodziny = str(nadgodziny)
+        self.delegacjaGodz = str(delegacjaGodz)
+        self.dodatkoweKoszty = str(dodatkoweKoszty)
+        self.uwagi = str(uwagi)
+
+    def wynik(self, text):
+        self.wynikDziałania = text
+
+    def printPdf(self):
+        
+        self.canvas = canvas.Canvas("raport.pdf", pagesize=letter)
+        self.canvas.setLineWidth(.3)
+        self.canvas.setFont('Helvetica', 20)
+
+        self.canvas.drawString(275,730,'RAPORT')
+        self.canvas.setFont('Helvetica', 16)
+        self.canvas.drawString(150,700,'System estymowanych pensji pracowników')
+        self.canvas.line(50,695,550,695)
+        self.canvas.setFont('Vera', 12)
+        self.canvas.drawString(500,750,str(self.date))
+        self.canvas.drawString(510,738, self.hourP)
+        self.canvas.line(480,735,580,735)
+        self.canvas.line(480,763,580,763)
+        self.canvas.drawString(90,670,"Stanowisko: ")
+        self.canvas.drawString(400, 670, self.stanowisko)
+        self.canvas.line(50,660,550,660)
+        self.canvas.drawString(90,640,"Wymiar Pracy: ")
+        self.canvas.drawString(400, 640, self.wymiarPracy)
+        self.canvas.line(50,630,550,630)
+        self.canvas.drawString(90,610,"Lata Pracy: ")
+        self.canvas.drawString(400, 610, self.lataPracy)
+        self.canvas.line(50,600,550,600)
+        self.canvas.drawString(90,580,"Nadgodziny: ")
+        self.canvas.drawString(400, 580, self.nadgodziny)
+        self.canvas.line(50,570,550,570)
+        self.canvas.drawString(90,550,"Delegacje: ")
+
+        self.canvas.drawString(90,520,"Liczba godzin w delegacji: ")
+        self.canvas.drawString(400, 520, self.delegacjaGodz)
+        self.canvas.drawString(90,490,"Dodatkowe koszty poniesione przez pracownika: ")
+        self.canvas.drawString(400, 490, self.dodatkoweKoszty)
+        self.canvas.line(50,480,550,480)
+
+        self.canvas.drawString(90,460,"Potrącenia i dodatki: ")
+        self.canvas.drawString(90,430,"Bilans pochwał/uwag: ")
+        self.canvas.drawString(400,430, self.uwagi)
+        self.canvas.line(50,420,550,420)
+        self.canvas.line(50,415,550,415)
+
+        #################################
+        if (self.wynikDziałania != ""):
+            lista = self.wynikDziałania.split('\n')
+            wysokosc = 380
+            for linia in lista[11:]:
+                if linia.endswith(".0"):
+                    linia = linia [:-2]
+                self.canvas.drawString(90, wysokosc, str(linia))
+                wysokosc -= 30
+
+
+
+        self.canvas.save()
+
+
+
+
