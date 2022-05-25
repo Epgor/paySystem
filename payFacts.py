c8df824ca76e5295f694dc3da8540cc9e9435db5
Epgor <61190141+Epgor@users.noreply.github.com>
2022-05-25 14:17:15 +0200

    raport w pdf

--- payFacts.py
+++ payFacts.py
@@ -5,26 +5,60 @@ class LataPracy(Fact):
     """lata pracy w firmie"""
     pass
 
-class PensjaNaGodzine(Fact):
-    pass
-
-class GodzinPracy(Fact):
-    pass
-
-class Premie(Fact):
+class Nadgodziny(Fact):
     pass
 
 class Pochwala(Fact):
     pass
 
-class Szkolenia(Fact):
-    pass
-
-class Wyksztalcenie(Fact):
-    pass
-
-class Urlop(Fact):
-    pass
-
 class DoswiadczenieWBranzy(Fact):
     pass
+
+class Stanowisko(Fact):
+    pass
+
+class Etat(Fact):
+    pass
+
+class DelegacjaGodz(Fact):
+    pass
+
+class DelegacjaProc(Fact):
+    pass
+
+class DelegacjaDod(Fact):
+    pass
+
+class Staz(Fact):
+    stawka = 20
+    nadgodzina = stawka * 1.5
+    delegacja = stawka * 8
+
+class Junior(Fact):
+    stawka = 50
+    nadgodzina = stawka * 1.5
+    delegacja = stawka * 8
+
+class Mid(Fact):
+    stawka = 75
+    nadgodzina = stawka * 1.5
+    delegacja = stawka * 8
+
+class Senior(Fact):
+    stawka = 125
+    nadgodzina = stawka * 1.5
+    delegacja = stawka * 8
+
+
+class Caly(Fact):
+    godzin = 160
+
+class TrzyCwierc(Fact):
+    godzin = 120
+
+class Polowa(Fact):
+    godzin = 80
+
+class Cwierc(Fact):
+    godzin = 40
+
